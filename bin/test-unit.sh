0c155339c1c9a00ec1bcfe6dd9385b4d0fe2035b
Alex Robinson <github@alex.fu2k.org>
2017-07-24 21:45:24 +0100

    Stash middleware tests

--- bin/test-unit.sh
+++ bin/test-unit.sh
@@ -1,3 +1,8 @@
 #!/usr/bin/env bash
 
-node_modules/ava/cli.js --verbose spec/**/*.unit.spec.js lib/**/*.unit.spec.js
+# node_modules/ava/cli.js --verbose lib/**/*.unit.spec.js
+# node_modules/ava/cli.js --verbose $TESTS
+
+TESTS=$(find lib -name *.unit.spec.js | xargs echo)
+node_modules/.bin/multi-tape -p=10 $TESTS | node_modules/.bin/tap-spec
+# multi-tape
