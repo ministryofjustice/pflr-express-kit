ce051d81feeda5f85080c063b7a39b3dc514b9e7
Alex Robinson <github@alex.fu2k.org>
2017-07-25 10:54:19 +0100

    Move testing of notFound handler out of server spec

--- bin/test-unit.sh
+++ bin/test-unit.sh
@@ -1,8 +1,3 @@
 #!/usr/bin/env bash
 
-# node_modules/ava/cli.js --verbose lib/**/*.unit.spec.js
-# node_modules/ava/cli.js --verbose $TESTS
-
-TESTS=$(find lib -name *.unit.spec.js | xargs echo)
-node_modules/.bin/multi-tape -p=10 $TESTS | node_modules/.bin/tap-spec
-# multi-tape
+node_modules/.bin/multi-tape -p=10 lib/**/*.unit.spec.js | node_modules/.bin/tap-spec
