6b4463e16cbdb043780c2c3ef29d6e042d121f8a
Alex Robinson <github@alex.fu2k.org>
2017-07-27 12:47:14 +0100

    Fill in a11y stubs

--- bin/test-a11y-ci.sh
+++ bin/test-a11y-ci.sh
@@ -1,3 +1,72 @@
-#!/usr/bin/env bash
+#!/bin/sh
 
-echo 'test-a11y-ci.sh stub'
+if [[ $1 == "-h" || $1 == "--help" ]]
+then
+  read -d '' USAGE <<- EndOfMessage
+		functional-tests-run.sh
+		=======================
+
+		Runs a11y tests on app running a docker container
+
+		Used by Jenkins jobs:
+		  https://ci.service.dsd.io/view/family%20justice/job/BUILD-cait
+
+		The following environment variables must be set:
+
+		  JOB_NAME
+
+		  BUILD_NUMBER
+
+		The following environment variables can also be set:
+
+		  BASE_URL
+		  base url to run the tests against
+		  - must include protocol, port and credentials (if necessary)
+
+		  REPORTS
+		  directory on host where reports should be written to
+		  - must be an absolute path
+
+		  SKIP_A11Y
+		  whether to run a11y tests or not
+EndOfMessage
+
+  echo "$USAGE"
+  exit 0
+fi
+
+if [ "$JOB_NAME" == "" ]
+then
+  echo Environment variable JOB_NAME must be set
+  exit 1
+fi
+if [ "$BUILD_NUMBER" == "" ]
+then
+  echo Environment variable BUILD_NUMBER must be set
+  exit 1
+fi
+
+if [ "$SKIP_A11Y" != "true" ]
+then
+  DOCKERTAG=$(echo $JOB_NAME-$BUILD_NUMBER | tr '[:upper:]' '[:lower:]')
+
+  APP=$DOCKERTAG-app
+  docker build -t $APP .
+
+  #### If no environment url passed, spin the app up
+  if [ -n $BASE_URL ]
+  then
+    docker run --name $APP -d $APP
+    APP_IP=$(docker inspect --format '{{.NetworkSettings.IPAddress}}' $APP)
+  fi
+
+  if [ "$REPORTS" != "" ]
+  then
+    REPORTS_VOLUME="-v $REPORTS:/usr/app/reports"
+    echo "Reports will be output to $REPORTS"
+  fi
+  # Now run the tests
+  A11Y=$DOCKERTAG-a11y
+  docker run $REPORTS_VOLUME --name $A11Y $APP yarn test:a11y
+
+fi
--- bin/test-a11y.sh
+++ bin/test-a11y.sh
@@ -1,3 +1,46 @@
 #!/usr/bin/env bash
 
-echo 'test-a11y.sh stub'
+if [ -n "$1" ]
+then
+  TARGET=$1
+else
+  IP_ADDRESS=$(ifconfig | sed -En 's/127.0.0.1//;s/.*inet (addr:)?(([0-9]*\.){3}[0-9]*).*/\2/p')
+  IP_ADDRESS=$(echo $IP_ADDRESS | sed 's/ .*//')
+  TARGET=http://$IP_ADDRESS:3000
+fi
+
+echo Checking $TARGET is reachable
+
+exit 0
+
+curl -s $TARGET > /dev/null
+if [ "$?" != "0" ]
+then
+  echo $TARGET is not reachable
+  exit 1
+fi
+
+echo Running a11y checks against $TARGET
+
+node_modules/.bin/a11ym -o reports/a11ym --no-verbose $TARGET
+
+ERRORS=$(node -e "const results = require('./reports/a11ym/statistics.json'); console.log(results.reduce((sum, result) => sum + result.errorCount, 0))")
+
+if [ "$ERRORS" != "0" ]
+then
+  echo "There were $ERRORS accessibility errors - check reports/a11ym/statistics.json for details"
+  exit 1
+fi
+
+
+# items.reduce((a, b) => { a + b.errorCount }, 0)
+
+
+# results.map(r => r.errorCount).reduce((sum, value) => sum + value, 0)
+
+# results.reduce((sum, result) => sum + result.errorCount, 0)
+
+
+# .reduce(function(sum, value) {
+#   return sum + value;
+# }, 0);
