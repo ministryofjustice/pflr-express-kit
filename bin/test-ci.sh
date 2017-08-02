371596af3be8faa57f36ed4d304457575f639380
Alex Robinson <github@alex.fu2k.org>
2017-08-02 15:12:13 +0100

    Allow test-ci to be invoked when symlinked

--- bin/test-ci.sh
+++ bin/test-ci.sh
@@ -1,5 +1,20 @@
 #!/bin/sh
 
-sh test-unit-ci.sh
-# sh test-a11y-ci.sh
-sh test-functional-ci.sh
+HERE=$0
+SCRIPTPATH=$HERE
+
+SYMLINKPATH=$(ls -l $SCRIPTPATH | awk '{print $11}')
+if [ "$SYMLINKPATH" != "" ]
+  then
+  if [ "$SYMLINKPATH" == "../pflr-express-kit/bin/test-ci.sh" ]
+    then
+    SCRIPTPATH=$(dirname $HERE)/$SYMLINKPATH
+    else
+    SCRIPTPATH=$SYMLINKPATH
+  fi
+fi
+SCRIPTPATH=$(echo $SCRIPTPATH | sed 's/\/test-ci.sh//')
+
+sh $SCRIPTPATH/test-unit-ci.sh
+# sh $SCRIPTPATH/test-a11y-ci.sh
+sh $SCRIPTPATH/test-functional-ci.sh
