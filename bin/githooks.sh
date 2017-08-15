270c91ed5526e573a167bffd2e6010364e8b5361
Alex Robinson <github@alex.fu2k.org>
2017-08-15 16:05:51 +0100

    Add common githooks and commands

---
+++ bin/githooks.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+git config core.hooksPath node_modules/pflr-express-kit/githooks
--- bin/postinstall.sh
+++ bin/postinstall.sh
@@ -1,3 +1,8 @@
 #!/usr/bin/env bash
 
+if [ -d .git ] && [ -x \"$(command -v git)\" ]
+  then npm run githooks
+  else echo 'git not installed'
+fi
+
 npm-run-all -p build
