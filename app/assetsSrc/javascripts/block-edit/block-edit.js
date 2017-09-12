eb63f00de32c743b88a8dd272048a4a836d86bfd
Alex Robinson <github@alex.fu2k.org>
2017-09-12 01:04:37 +0100

    Enhance handling of multipl blocks and routes in flowchart and edit views

--- app/assetsSrc/javascripts/block-edit/block-edit.js
+++ app/assetsSrc/javascripts/block-edit/block-edit.js
@@ -1,7 +1,7 @@
 jQuery(document).ready(function () {
   jQuery('[data-block-name]').each(function () {
     var dataBlockEdit = jQuery(this).closest('[data-block-name]')
-    var block = dataBlockEdit.attr('data-block-name')
+    var block = dataBlockEdit.attr('data-block-name').replace(/_\d+$/, '')
     jQuery(this).prepend('<a href="/admin/block/' + block + '" target="_blank" class="block-edit" title="' + block + '"><span>✎</span></a>')
     jQuery('.block-edit', this)
       .on('mouseover', function () {
