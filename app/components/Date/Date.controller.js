385612bcc1395be49b6618638d48a365be2a24a5
Alex Robinson <github@alex.fu2k.org>
2017-11-10 20:58:22 +0000

    Add date_type for Dates

--- app/components/Date/Date.controller.js
+++ app/components/Date/Date.controller.js
@@ -1,5 +1,6 @@
 const jsonschema = require('jsonschema')
 const validator = new jsonschema.Validator()
+const { getBlockProp } = require('pflr-express-kit/lib/get-block')
 
 // instance: '2016--23'
 // name: 'format'
@@ -7,8 +8,36 @@ const validator = new jsonschema.Validator()
 
 const validate = (values, schema, name) => {
   let { day, month, year } = values
+  let noValues
+  if (!day && !month && !year) {
+    noValues = true
+  }
   day = day ? ('0' + day).replace(/0*(\d\d)$/, '$1') : day
   month = month ? ('0' + month).replace(/0*(\d\d)$/, '$1') : month
+
+  if (!noValues) {
+    const dateType = schema.date_type || 'day-month-year'
+    if (!dateType.includes('day')) {
+      day = '01'
+    }
+    if (!dateType.includes('month')) {
+      month = '01'
+    }
+    if (!dateType.includes('year')) {
+      year = '2000'
+    }
+  }
+
+  // alt
+  // if (schema.day === false) {
+  //   day = '01'
+  // }
+  // if (schema.month === false) {
+  //   month = '01'
+  // }
+  // if (schema.year === false) {
+  //   year = '2000'
+  // }
 
   schema.type = 'string'
   schema.format = 'date'
@@ -45,10 +74,30 @@ const validate = (values, schema, name) => {
 }
 
 const getDisplayValue = (name, separator, componentValues, vals, fieldValues) => {
-  if (!componentValues.day || !componentValues.month || !componentValues.year) {
+  // handle multiple route elements
+  name = name.replace(/.*_\d+_/, '')
+  const dateType = getBlockProp(name, 'date_type', 'day-month-year')
+  const day = dateType.includes('day')
+  const month = dateType.includes('month')
+  const year = dateType.includes('year')
+  // alt
+  // const day = getBlockProp(name, 'day', true)
+  // const month = getBlockProp(name, 'month', true)
+  // const year = getBlockProp(name, 'year', true)
+  if ((day && !componentValues.day) || (month && !componentValues.month) || (year && !componentValues.year)) {
     return
   }
-  return `${componentValues.day}/${componentValues.month}/${componentValues.year}`
+  let displayValues = []
+  if (day) {
+    displayValues.push(componentValues.day)
+  }
+  if (month) {
+    displayValues.push(componentValues.month)
+  }
+  if (year) {
+    displayValues.push(componentValues.year)
+  }
+  return displayValues.join('/')
 }
 
 module.exports = {
